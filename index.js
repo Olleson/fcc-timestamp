466d9089bcbae4f6ab6c43337751c07c7e602b51
Olivia Xu <xu.olivia774@gmail.com>
2022-07-20 20:35:33 +0200

    aaaaa

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,15 +19,45 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
-
-
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+let listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// your first API endpoint... 
+app.get("/api/:date", function (req, res) {
+  res.json(getDate(req.params.date));
+});
+
+app.get("/api/", (req, res) => {
+  res.json(getDate(0));
+})
+
+const getDate = (date) => {
+  if (date === 0) {
+    let currentDate = new Date();
+    return {
+      unix: Date.parse(currentDate), 
+      utc: currentDate.toGMTString() 
+    }
+  }
+
+  let utcDate;
+  let newDate = date.split('-').map(unit => Number(unit));
+  if (newDate.includes(NaN)) {
+    return {
+      error: "Invalid Date" 
+    }
+  }
+
+  if (newDate.length > 1) {
+    utcDate = new Date(Date.UTC(newDate[0], newDate[1] - 1, newDate[2])); // year, month, day
+  } else {
+    utcDate = new Date(newDate[0]);
+  }
+  console.log(`returning {unix: ${Date.parse(utcDate)}, utc: ${utcDate.toGMTString()}}`);
+  return {
+    unix: Date.parse(utcDate),
+    utc: utcDate.toGMTString()
+  }
+}
